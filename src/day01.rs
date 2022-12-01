884f9055d2700a7d143e8e4835690b07ed562852
s.kovbasa <s.kovbasa@smartweb.com.ua>
2022-12-01 17:11:15 +0200

    Use oneliner))

--- src/day01.rs
+++ src/day01.rs
@@ -1,17 +1,13 @@
 pub const URL: &str = "https://adventofcode.com/2022/day/1/input";
 
 pub fn solve1(text: &str) -> u32 {
-    let elves = text.split("\n\n");
-    let elves_total: Vec<u32> = elves.map(
+    text.split("\n\n").map(
         |x| x.lines().map(|l| l.parse::<u32>().unwrap()).sum()
-    ).collect();
-
-    *elves_total.iter().max().unwrap()
+    ).max().unwrap()
 }
 
 pub fn solve2(text: &str) -> u32 {
-    let elves = text.split("\n\n");
-    let mut elves_total: Vec<u32> = elves.map(
+    let mut elves_total: Vec<u32> = text.split("\n\n").map(
         |x| x.lines().map(|l| l.parse::<u32>().unwrap()).sum()
     ).collect();
 
